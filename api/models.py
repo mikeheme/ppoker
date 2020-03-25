429248cbf305adb6199b8fea0e0cf13eb9c08d4f
Mike Hemelberg <mikeheme@gmail.com>
2020-03-25 00:42:16 -0700

    Card deck class

--- api/models.py
+++ api/models.py
@@ -1,7 +1,9 @@
+import itertools
 import uuid
+import random
 
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Room(models.Model):
@@ -9,5 +11,20 @@ class Room(models.Model):
     name = models.CharField(max_length=150, null=True)
     is_active = models.BooleanField(default=False)
     users = models.ManyToManyField(User, related_name='rooms')
+    deck = models.CharField(max_length=1000, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+
+class CardDeck:
+    deck = None
+
+    def __init__(self):
+        self.initialize()
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def initialize(self):
+        self.deck = list(itertools.product(
+            range(1, 14), ['Spade', 'Heart', 'Diamond', 'Club']))
